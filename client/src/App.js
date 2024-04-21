8a8cbe24aa803410a9b9655e69d58268637fc4fb
Sakshi Maheshwari <sakshicoder800@gmail.com>
2024-04-21 12:44:02 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './components/footer/footer';
+function App() {
+  return (
+    <>
+    <Footer/>
+    </>
+    
+
+  );
+}
+
+export default App;
